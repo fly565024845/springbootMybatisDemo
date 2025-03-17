44f73165f9f85d8395c0b484372c2aac1e2553c8
fly565024845 <40479544+fly565024845@users.noreply.github.com>
2025-03-17 16:20:57 +0800

    Update mysqltest.sql

--- mysqltest.sql
+++ mysqltest.sql
@@ -6,4 +6,12 @@ CREATE TABLE student (
     birthdate DATE
 );
 
-insert into student ("fly1","男"，40，NOW())
+INSERT INTO student (NAME,gender,age,birthdate) VALUES ("fly1","男",21,NOW());
+INSERT INTO student (NAME,gender,age,birthdate) VALUES ("fly2","男",22,NOW());
+INSERT INTO student (NAME,gender,age,birthdate) VALUES ("fly3","男",23,NOW());
+INSERT INTO student (NAME,gender,age,birthdate) VALUES ("fly4","男",24,NOW());
+INSERT INTO student (NAME,gender,age,birthdate) VALUES ("fly4","男",25,NOW());
+INSERT INTO student (NAME,gender,age,birthdate) VALUES ("fly5","男",26,NOW());
+INSERT INTO student (NAME,gender,age,birthdate) VALUES ("fly6","男",27,NOW());
+
+SELECT * FROM student;
